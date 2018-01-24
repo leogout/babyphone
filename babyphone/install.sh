3ab56dcb389840f29609b927796c5a2eba7833a4
Léo Richerd <leogour@gmail.com>
2018-01-24 11:04:21 +0100

    added some sudos to make it work

--- babyphone/install.sh
+++ babyphone/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get update
 
 sudo apt-get install jq
 sudo apt-get install npm
@@ -9,9 +10,9 @@ sudo apt-get install subversion libv4l-dev libjpeg8-dev imagemagick
 sudo ln -s /usr/include/linux/videodev2.h /usr/include/linux/videodev.h
 svn co https://svn.code.sf.net/p/mjpg-streamer/code/
 cd code/mjpg-streamer
-make USE_LIBV4L2=true clean all
-make DESTDIR=/usr install
+sudo make USE_LIBV4L2=true clean all
+sudo make DESTDIR=/usr install
 cd ../../
-rm -r ./code
+sudo rm -r ./code
 
 npm install -g ngrok
