dff2409077ca6519833b4ab268e21e2478855a24
Léo Richerd <leogour@gmail.com>
2018-01-24 10:38:21 +0100

    added mjpeg_streamer installation and run

--- babyphone/install.sh
+++ babyphone/install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-apt-get install jq
-apt-get install npm
+sudo apt-get install jq
+sudo apt-get install npm
+sudo apt-get install uuid-runtime
+
+#
+sudo apt-get install subversion libv4l-dev libjpeg8-dev imagemagick
+ln -s /usr/include/linux/videodev2.h /usr/include/linux/videodev.h
+svn co https://svn.code.sf.net/p/mjpg-streamer/code/
+cd code/mjpg-streamer
+make USE_LIBV4L2=true clean all
+make DESTDIR=/usr install
 
 npm install -g ngrok
--- babyphone/run.sh
+++ babyphone/run.sh
@@ -3,32 +3,34 @@
 # General
 # SERVER_URL='54.37.11.19'
 SERVER_URL='127.0.0.1:8000'
+LD_LIBRARY_PATH=/usr/local/lib
 
 echo "Looking for the uniq ID of the babyphone"
 
+# teste si l'id unique a déjà été généré
 if [ ! -f ./ID ]; then
-  echo "ID not found, generating an uniq ID for the babyphone"
+  echo "ID not found, generating an uniq id for the babyphone"
   ID=$(uuidgen)
-  echo "Saving it into ./ID"
+  echo "Saving id into ./ID"
   echo $ID > ID
 else
   ID=$(head -n 1 ./ID)
 fi
 
-echo $ID
+echo "Generated id: $ID"
 
+# teste si ngrok est déjà en marche
 if pgrep -x "ngrok" > /dev/null
 then
     echo "ngrok is already running"
 else
     echo "Starting ngrok"
 
-    ngrok http --log=stdout 80 > /dev/null &
-
+    ngrok http --log=stdout 8080 > /dev/null &
     sleep 5
 fi
 
-echo "Gathering ngrok tunnel url"
+echo "Gathering ngrok generated url"
 
 URL=$(curl http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url')
 
@@ -40,3 +42,7 @@ curl -X POST \
   -H "content-type: application/x-www-form-urlencoded" \
   -H "postman-token: 82f18ff1-fd24-910a-b7a8-93d85ef4fb55" \
   -d "url=$URL&serial=$ID"
+
+mkdir /tmp/stream
+raspistill --nopreview -w 640 -h 480 -q 5 -o /tmp/stream/pic.jpg -tl 500 -t 0
+mjpg_streamer -i "input_file.so -f /tmp/stream -n pic.jpg" -o "output_http.so -w ./www"
