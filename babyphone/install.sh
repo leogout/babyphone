3ec13bce284d7e7a2cb53ecd1f3c5d8df24e9628
Léo Richerd <leogour@gmail.com>
2018-01-24 10:44:55 +0100

    added removal of compiled files

--- babyphone/install.sh
+++ babyphone/install.sh
@@ -6,10 +6,12 @@ sudo apt-get install uuid-runtime
 
 #
 sudo apt-get install subversion libv4l-dev libjpeg8-dev imagemagick
-ln -s /usr/include/linux/videodev2.h /usr/include/linux/videodev.h
+sudo ln -s /usr/include/linux/videodev2.h /usr/include/linux/videodev.h
 svn co https://svn.code.sf.net/p/mjpg-streamer/code/
 cd code/mjpg-streamer
 make USE_LIBV4L2=true clean all
 make DESTDIR=/usr install
+cd ../../
+rm -r ./code
 
 npm install -g ngrok
